d2654844d6a9689281eec1ccb36626e4a500edfb
Linastraa <linastraaa@gmail.com>
2024-05-12 09:00:43 +0300

    us_0010 code

--- src/main/java/sda/capstone/Pages/RemoteUnitsPage.java
+++ src/main/java/sda/capstone/Pages/RemoteUnitsPage.java
@@ -2,11 +2,15 @@ package sda.capstone.Pages;
 
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Wait;
 import sda.capstone.ActionsBot;
 import sda.capstone.PageBase;
 import sda.capstone.PropertiesReader;
+
+import java.util.List;
 
 
 public class RemoteUnitsPage extends PageBase {
@@ -18,6 +22,9 @@ public class RemoteUnitsPage extends PageBase {
     private final By addNewRemoteUnitButton = By.xpath("//button[contains(., 'Add New Remote Unit')]");
 
 //    private final By editRemoteUnit = By.xpath("//b[contains(., 'editTeam4Test')]");
+
+    //us_0010 locator
+    private final By unitsList = By.tagName("b");
 
 
     @Step("Given I am on the Remote Units page")
@@ -40,8 +47,12 @@ public class RemoteUnitsPage extends PageBase {
 //    }
 
 
+//us_0010 code
+    @Step("Then I will get a list of the registered remote units")
+    public List<WebElement> getUnitsList() {
+        wait.until(d->driver.findElement(unitsList).isDisplayed());
 
-
-
+        return driver.findElements(unitsList);
+    }
 
 }
--- src/test/java/sda/capstone/us0009/EditDepartmentNameAndTypeTest.java
+++ src/test/java/sda/capstone/us0009/EditDepartmentNameAndTypeTest.java
@@ -10,63 +10,102 @@ import sda.capstone.listener.CustomListener;
 
 @Listeners(CustomListener.class)
 public class EditDepartmentNameAndTypeTest extends TestBase {
-    public String username = "testevolve12@testevolve.com";
-    public String password = "DJK7wyf8_ZpG464";
+    public String username = "t4@testevolve.com";
+    public String password = "ByDckIStx4Yet14";
 
     @Step("Testing the functionality of editing Department name and type")
-    @Test
+    @Test(priority = 1)
     public void editDepartmentNameAndTypeTest() {
 
         StartPage startPage = new StartPage(driver,bot,wait);
-        startPage.goTo().clickLoginButton().login(username,password);
+
+        startPage.
+                goTo().
+                clickLoginButton().
+                login(username,password);
 
         DepartmentsPage departmentsPage = new DepartmentsPage(driver,bot,wait);
 
-        String text = departmentsPage.goTo().ClickOnDepartmentName().ClickOnEdit().
-                EditTheDepartmentName().EditTheDepartmentType().ClickingOnSaveButton().
+        String text =
+                departmentsPage.
+                goTo().
+                ClickOnDepartmentName().
+                ClickOnEdit().
+                EditTheDepartmentName().
+                EditTheDepartmentType().
+                ClickingOnSaveButton().
                 GetTheSuccessMessage();
 
         Assert.assertEquals(text,"Changes successfully saved");
     }
 
     @Step("Testing the functionality of leaving Department name empty")
-    @Test
+    @Test(priority = 2)
     public void editWithEmptyDepartmentNameTest() {
 
         StartPage startPage = new StartPage(driver,bot,wait);
-        startPage.goTo().clickLoginButton().login(username,password);
+
+        startPage.
+                goTo().
+                clickLoginButton().
+                login(username,password);
 
         DepartmentsPage departmentsPage = new DepartmentsPage(driver,bot,wait);
 
-        String text = departmentsPage.goTo().ClickOnDepartmentName().ClickOnEdit().LeaveTheDepartmentNameEmpty().
-                ClickingOnSaveButton().GetTheFailureMessage();
+        String text =
+                departmentsPage.
+                goTo().
+                ClickOnDepartmentName().
+                ClickOnEdit().
+                LeaveTheDepartmentNameEmpty().
+                ClickingOnSaveButton().
+                GetTheFailureMessage();
+
         Assert.assertEquals(text,"Please enter a name for department");
     }
 
     @Step("Testing the functionality of leaving Department type empty")
-    @Test
+    @Test(priority = 3)
     public void editWithEmptyDepartmentTypeTest() {
 
         StartPage startPage = new StartPage(driver,bot,wait);
-        startPage.goTo().clickLoginButton().login(username,password);
+        startPage.
+                goTo().
+                clickLoginButton().
+                login(username,password);
 
         DepartmentsPage departmentsPage = new DepartmentsPage(driver,bot,wait);
 
-        String text = departmentsPage.goTo().ClickOnDepartmentName().ClickOnEdit().LeaveTheDepartmentTypeEmpty().
-                ClickingOnSaveButton().GetTheFailureMessage();
+        String text =
+                departmentsPage.
+                goTo().
+                ClickOnDepartmentName().
+                ClickOnEdit().
+                LeaveTheDepartmentTypeEmpty().
+                ClickingOnSaveButton().
+                GetTheFailureMessage();
+
         Assert.assertEquals(text,"Please select a type for department");
     }
 
     @Step("Testing the functionality of deleting the department")
-    @Test
+    @Test(priority = 4)
     public void deleteTheDepartmentTest() {
 
         StartPage startPage = new StartPage(driver,bot,wait);
-        startPage.goTo().clickLoginButton().login(username,password);
+        startPage.
+                goTo().
+                clickLoginButton().
+                login(username,password);
 
         DepartmentsPage departmentsPage = new DepartmentsPage(driver,bot,wait);
 
-        String text = departmentsPage.goTo().ClickOnDepartmentName().ClickOnEdit().DeleteTheDepartment();
+        String text = departmentsPage.
+                goTo().
+                ClickOnDepartmentName().
+                ClickOnEdit().
+                DeleteTheDepartment();
+
         Assert.assertEquals(text,"Departments");
     }
 
