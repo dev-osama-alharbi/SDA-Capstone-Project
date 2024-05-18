ad9c01c3421b1283fd961fbb5a55b6b33bb57692
Manarahmed99 <manar6hmed@gmail.com>
2024-05-18 23:46:46 +0300

    US_0010 User Service Requests

---
+++ src/test/java/sda/capstone/API/test/us0010/TC0056.java
@@ -0,0 +1,39 @@
+package sda.capstone.API.test.us0010;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import sda.capstone.API.ApiWithCookieHeaderBase;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class TC0056 extends ApiWithCookieHeaderBase {
+
+    @Test
+    public void getUserByIDTest()throws JsonProcessingException {
+
+        HashMap<String,String> pathParams = new HashMap<>();
+        pathParams.put("first","user");
+        pathParams.put("user_id","1787");
+
+
+        spec.pathParams(pathParams);
+
+        Response response = given(spec).get("/a3m/auth/api/v1/{first}/{user_id}");
+        response.prettyPrint();
+
+
+        response
+                .then()
+                .statusCode(200)
+                .body(  "id", equalTo(1787),
+                        "name",equalTo("SDA"),
+                        "lastname",equalTo("API"));
+
+
+
+    }
+}
