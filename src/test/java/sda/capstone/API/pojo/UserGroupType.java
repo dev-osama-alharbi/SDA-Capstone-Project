b3826c306b1f9204050b06e6d82da8d4c645d8ec
Areej <areej262626@gmail.com>
2024-05-19 11:01:31 +0300

    --

---
+++ src/test/java/sda/capstone/API/pojo/UserGroupType.java
@@ -0,0 +1,21 @@
+package sda.capstone.API.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class UserGroupType {
+
+    private Integer id;
+    private String name;
+    private String description;
+
+}
+
+
