8f055152cc393341bd79f10fd7bbc114d7b0334e
Osama AL-Harbi <me@osama-alharbi.sa>
2024-05-19 09:51:17 +0300

    Update ObjectMapperUtils.java

--- src/test/java/sda/capstone/API/utilities/ObjectMapperUtils.java
+++ src/test/java/sda/capstone/API/utilities/ObjectMapperUtils.java
@@ -8,6 +8,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 
 public class ObjectMapperUtils {
+    public static <T> T convertJsonToJava(String strJson, Class<T> valueType) {//Generic method
+
+        try {
+            return new ObjectMapper().readValue(strJson, valueType);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 //    private static final ObjectMapper objectMapper = new ObjectMapper()
 //            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 //
@@ -22,23 +31,24 @@ public class ObjectMapperUtils {
 //    }
 //}
 
-    private static final ObjectMapper objectMapper = new ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    //TODO fix from US_0003%26000
+    // private static final ObjectMapper objectMapper = new ObjectMapper()
+    //         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-    public static <T> T convertJsonToJava(String jsonString, Class<T> cls) {
-        try {
-            JsonNode jsonNode = objectMapper.readTree(jsonString);
-            if (jsonNode.isArray()) {
-                // Handle array deserialization
-                return objectMapper.readValue(jsonString, objectMapper.getTypeFactory().constructArrayType(cls));
-            } else {
-                // Handle single object deserialization
-                return objectMapper.readValue(jsonString, cls);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            // Log the exception and rethrow it or return null
-            throw new RuntimeException("Error converting JSON to Java object", e);
-        }
-    }
+    // public static <T> T convertJsonToJava(String jsonString, Class<T> cls) {
+    //     try {
+    //         JsonNode jsonNode = objectMapper.readTree(jsonString);
+    //         if (jsonNode.isArray()) {
+    //             // Handle array deserialization
+    //             return objectMapper.readValue(jsonString, objectMapper.getTypeFactory().constructArrayType(cls));
+    //         } else {
+    //             // Handle single object deserialization
+    //             return objectMapper.readValue(jsonString, cls);
+    //         }
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //         // Log the exception and rethrow it or return null
+    //         throw new RuntimeException("Error converting JSON to Java object", e);
+    //     }
+    // }
 }
