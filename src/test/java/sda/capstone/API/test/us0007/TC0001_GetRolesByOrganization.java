413fbc4ad58fe9a12de570af4939c180fd24d618
osama <me@osama-alharbi.sa>
2024-05-22 12:27:46 +0300

    check and fix API part

--- src/test/java/sda/capstone/API/test/us0007/TC0001_GetRolesByOrganization.java
+++ src/test/java/sda/capstone/API/test/us0007/TC0001_GetRolesByOrganization.java
@@ -3,6 +3,7 @@ package sda.capstone.API.test.us0007;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import sda.capstone.API.APIVars;
 import sda.capstone.API.ApiBase;
 import sda.capstone.API.ApiWithCookieHeaderBase;
 import sda.capstone.API.pojo.AllRoles;
@@ -25,9 +26,12 @@ public class TC0001_GetRolesByOrganization extends ApiWithCookieHeaderBase {
         pathParams.put("p1","application");
         pathParams.put("p2","2");
         pathParams.put("p3","organization");
-        pathParams.put("p4","1715253779444611");
+        pathParams.put("p4","1716287440101561");
+//        pathParams.put("p4", APIVars.read().getOrganizationId()+"");
         pathParams.put("p5","role");
         spec.pathParams(pathParams);
+
+        System.out.println("APIVars.read().getOrganizationId() => "+APIVars.read().getOrganizationId());
 
         Response response =given(spec).get("/a3m/auth/api/{p1}/{p2}/{p3}/{p4}/{p5}");
         response.prettyPrint();
